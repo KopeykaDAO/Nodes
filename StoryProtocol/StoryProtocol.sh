15a85866e853f77434ae94e62d56dd723bc75c55
Ильичев Андрей Сергеевич <sternarsonist@gmail.com>
2024-08-31 11:50:32 +0600

    Update StoryProtocol.sh
    change quotes

--- StoryProtocol/StoryProtocol.sh
+++ StoryProtocol/StoryProtocol.sh
@@ -1,1 +1,1 @@
-echo "Гайд устарел. Вспользуйтесь новым: https://statuesque-eel-fab.notion.site/Story-Protocol-Node-19b66a105e8c4d59be6e9e9ec7f47b84"
+echo 'Гайд устарел. Вспользуйтесь новым: https://statuesque-eel-fab.notion.site/Story-Protocol-Node-19b66a105e8c4d59be6e9e9ec7f47b84'
