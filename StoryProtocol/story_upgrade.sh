42450f4a563bc9e55a6e298680807bc1f4fd780d
Логинов Семён <58379599+ShalomGH@users.noreply.github.com>
2024-09-26 23:55:53 +0600

    Update story_upgrade.sh

--- StoryProtocol/story_upgrade.sh
+++ StoryProtocol/story_upgrade.sh
@@ -8,4 +8,10 @@ rm story/story
 wget -O story.tar.gz $(curl -s https://api.github.com/repos/piplabs/story/releases/latest | jq .body | grep -oP '(?<=\[Linux 64-Bit Intel AMD \(x86_64\)\]\().*?(?=\))')
 tar --strip-components=1 -xzf story.tar.gz -C story
 rm story.tar.gz
+
+rm story/geth
+wget -O story-geth.tar.gz $(curl -s https://api.github.com/repos/piplabs/story-geth/releases/latest | jq .body | grep -oP '(?<=\[Linux 64-Bit Intel AMD \(x86_64\)\]\().*?(?=\))')
+tar --strip-components=1 -xzf story-geth.tar.gz -C geth
+rm story-geth.tar.gz
+
 sudo systemctl start story
