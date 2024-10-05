0f2ab9520c6bfabda2bd502e3045eb3adef12439
Логинов Семён <58379599+ShalomGH@users.noreply.github.com>
2024-10-05 19:54:22 +0600

    Update get_snapshot.sh

--- StoryProtocol/get_snapshot.sh
+++ StoryProtocol/get_snapshot.sh
@@ -1,12 +1,12 @@
-systemctl stop geth                         
-systemctl stop story
+sudo systemctl stop geth                         
+sudo systemctl stop story
 
 # Back up your validator state
 cp $HOME/.story/story/data/priv_validator_state.json $HOME/.story/priv_validator_state.json.backup
 
 # Delete previous geth chaindata and story data folders
-rm -rf $HOME/.story/geth/iliad/geth/chaindata
-rm -rf $HOME/.story/story/data
+sudo rm -rf $HOME/.story/geth/iliad/geth/chaindata
+sudo rm -rf $HOME/.story/story/data
 
 # Download story-geth and story snapshots
 wget -O geth_snapshot.lz4 https://snapshots2.mandragora.io/story/geth_snapshot.lz4
@@ -17,12 +17,12 @@ lz4 -c -d geth_snapshot.lz4 | tar -xv -C $HOME/.story/geth/iliad/geth
 lz4 -c -d story_snapshot.lz4 | tar -xv -C $HOME/.story/story
 
 # Delete downloaded story-geth and story snapshots
-rm -v geth_snapshot.lz4
-rm -v story_snapshot.lz4
+sudo rm -v geth_snapshot.lz4
+sudo rm -v story_snapshot.lz4
 
 # Restore your validator state
 cp $HOME/.story/priv_validator_state.json.backup $HOME/.story/story/data/priv_validator_state.json
 
 # Start your story-geth and story nodes
-systemctl start geth
-systemctl start story
+sudo systemctl start geth
+sudo systemctl start story
