8548c588304ccae77e49859cc75634bbcaab7f5a
Romanlsx <romaxomept@gmail.com>
2024-10-15 21:49:39 +0600

    Update peers_feeding.sh

--- StoryProtocol/peers_feeding.sh
+++ StoryProtocol/peers_feeding.sh
@@ -1,2 +1,5 @@
+sudo systemctl stop story geth
 peers=$(curl -sS https://lightnode-rpc-story.grandvalleys.com/net_info | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):\(.node_info.listen_addr)"' | awk -F ':' '{print $1":"$(NF)}' | paste -sd, -)
 sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$peers\"|" $HOME/.story/story/config/config.toml
+sudo systemctl restart geth story
+sudo journalctl -f -u story -u geth
