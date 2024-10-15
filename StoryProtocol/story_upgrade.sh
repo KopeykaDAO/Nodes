8cab4cdb6af09369044067bc2b86509f95c0cd89
Логинов Семён <58379599+ShalomGH@users.noreply.github.com>
2024-10-15 23:41:51 +0600

    Update story_upgrade.sh

--- StoryProtocol/story_upgrade.sh
+++ StoryProtocol/story_upgrade.sh
@@ -10,8 +10,7 @@ tar --strip-components=1 -xzf story.tar.gz -C story
 rm story.tar.gz
 
 rm geth/geth
-wget -O story-geth.tar.gz $(curl -s https://api.github.com/repos/piplabs/story-geth/releases/latest | jq -r '.assets[] | select(.name == "geth-linux-amd64") | .browser_download_url')
-tar --strip-components=1 -xzf story-geth.tar.gz -C geth
-rm story-geth.tar.gz
+wget -O geth $(curl -s https://api.github.com/repos/piplabs/story-geth/releases/latest | jq -r '.assets[] | select(.name == "geth-linux-amd64") | .browser_download_url')
+mv geth geth/
 
 sudo systemctl start story
