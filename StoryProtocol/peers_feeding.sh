fec136771b024922d751a19fc3c0bce4579a49f4
Romanlsx <romaxomept@gmail.com>
2024-10-15 21:45:13 +0600

    Create peers_feeding.sh

---
+++ StoryProtocol/peers_feeding.sh
@@ -0,0 +1,2 @@
+peers=$(curl -sS https://lightnode-rpc-story.grandvalleys.com/net_info | jq -r '.result.peers[] | "\(.node_info.id)@\(.remote_ip):\(.node_info.listen_addr)"' | awk -F ':' '{print $1":"$(NF)}' | paste -sd, -)
+sed -i -e "s|^persistent_peers *=.*|persistent_peers = \"$peers\"|" $HOME/.story/story/config/config.toml
