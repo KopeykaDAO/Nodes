047e1fb577c24fa15e678a309207efc485488bb1
Romanlsx <romaxomept@gmail.com>
2024-11-07 12:58:10 +0600

    Update new_story_upgrade.sh

--- StoryProtocol/new_story_upgrade.sh
+++ StoryProtocol/new_story_upgrade.sh
@@ -6,6 +6,7 @@ sudo systemctl stop story geth
 
 rm story/story
 wget -O story $(curl -s https://api.github.com/repos/piplabs/story/releases/latest | jq -r '.assets[] | select(.name == "story-linux-amd64") | .browser_download_url')
+mv story story/story
 
 rm geth/geth
 wget -O story-geth $(curl -s https://api.github.com/repos/piplabs/story-geth/releases/latest | jq -r '.assets[] | select(.name == "geth-linux-amd64") | .browser_download_url')
