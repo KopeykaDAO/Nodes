8a0be1a8c3b9e97990b947701de895074aac2999
Ильичев Андрей Сергеевич <sternarsonist@gmail.com>
2024-12-18 11:30:39 +0600

    Create node_exporter.sh

---
+++ monitoring/node_exporter.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if ! command -v curl &> /dev/null; then
+    sudo apt update
+    sudo apt install curl -y
+fi
+sleep 1
+
+cd $HOME && \
+wget https://github.com/prometheus/node_exporter/releases/download/v1.2.0/node_exporter-1.2.0.linux-amd64.tar.gz
+tar xvf node_exporter-1.2.0.linux-amd64.tar.gz
+rm node_exporter-1.2.0.linux-amd64.tar.gz
+sudo mv node_exporter-1.2.0.linux-amd64 node_exporter
+chmod +x $HOME/node_exporter/node_exporter
+mv $HOME/node_exporter/node_exporter /usr/bin
+rm -Rvf $HOME/node_exporter/
+
+sudo tee /etc/systemd/system/exporterd.service > /dev/null <<EOF
+[Unit]
+Description=node_exporter
+After=network-online.target
+[Service]
+User=$USER
+ExecStart=/usr/bin/node_exporter
+Restart=always
+RestartSec=3
+LimitNOFILE=65535
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable exporterd
+sudo systemctl restart exporterd
+
+sudo ufw allow 9100 
